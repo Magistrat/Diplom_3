4a96967931075e3c18bbd10e697990dadaade242
Magistrat <nisraton@gmail.com>
2023-10-03 20:35:05 +0300

    [feature] start chrome browser in base test

--- src/main/java/com/project/SettingsInterface.java
+++ src/main/java/com/project/SettingsInterface.java
@@ -1,9 +1,11 @@
+package com.project;
+
 import java.util.List;
 
 public interface SettingsInterface {
     public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/";
 
-    public static final List<String> listChromeOptions = List.of(
+    public static final List<String> LIST_CHROME_OPTIONS = List.of(
             "--no-sandbox",
             "--disable-dev-shm-usage",
             "--remote-allow-origins=*"
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,11 @@
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.project.SettingsInterface.BASE_URL;
+import static com.project.SettingsInterface.LIST_CHROME_OPTIONS;
 
 public abstract class BaseTest {
 
@@ -8,7 +13,13 @@ public abstract class BaseTest {
 
     @Before
     public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(LIST_CHROME_OPTIONS);
+        driver = new ChromeDriver(options);
 
+
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
     }
 
 
