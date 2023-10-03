c7d4d226f471d91badc04aeded7acf432c471a9c
Magistrat <nisraton@gmail.com>
2023-10-03 22:34:22 +0300

    [feature] create interface wits api settings

---
+++ src/main/java/com/api/SettingsApiInterface.java
@@ -0,0 +1,10 @@
+package com.api;
+
+public interface SettingsApiInterface {
+    // Настройки для работы с API
+
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+
+    public static final String REGISTER_USER_URL = "/api/auth/register";
+    public static final String DELETE_USER_URL = "/api/auth/register";
+}
