6a40a30f6eac21460502c55e468b7152cdca81db
Magistrat <nisraton@gmail.com>
2023-10-03 22:43:43 +0300

    [feature] create pojo for send request and receive response

---
+++ src/main/java/com/api/pojo/register/RegisterPositiveResponseUserPojo.java
@@ -0,0 +1,8 @@
+package com.api.pojo.register;
+
+public class RegisterPositiveResponseUserPojo {
+    // Pojo для регистрации пользователя (получение JSON). Для ключа user в ответе
+
+    private String email;
+    private String name;
+}
