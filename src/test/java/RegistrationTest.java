a17bb0f890e19ecd42e3b016f0a9a5db95375dd6
Magistrat <nisraton@gmail.com>
2023-10-03 20:48:32 +0300

    [feature] create RegistrationTest

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,21 @@
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static com.project.SettingsInterface.REGISTER_URL;
+
+public class RegistrationTest extends BaseTest {
+
+    @Before
+    public void openRegistrationPage(){
+        driver.get(REGISTER_URL);
+    }
+
+    @Test
+    @DisplayName("Успешная регистрация")
+    public void positiveRegistration(){
+
+    }
+
+}
