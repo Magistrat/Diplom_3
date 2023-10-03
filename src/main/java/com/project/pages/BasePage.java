8ef41dd36b5bf947bb44601d2eacb08e10437a47
Magistrat <nisraton@gmail.com>
2023-10-03 21:52:23 +0300

    [feature] add base method for check element visibility and fill text to element

--- src/main/java/com/project/pages/BasePage.java
+++ src/main/java/com/project/pages/BasePage.java
@@ -1,12 +1,29 @@
 package com.project.pages;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static com.project.SettingsInterface.EXPLICITLY_TIMEOUT;
 
 public abstract class BasePage {
     private final WebDriver driver;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    @Step("Элемент отображается на странице и присутствует в DOM")
+    public void elementHasVisibility(By locator){
+        new WebDriverWait(driver, EXPLICITLY_TIMEOUT).until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    @Step("Ввести текст в элемент")
+    public void fillToInput(By locator, String textInput){
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(textInput);
     }
 
 }
