4069945fe4d9b6985d068237bfb5954f53131ef5
Magistrat <nisraton@gmail.com>
2023-10-03 22:09:21 +0300

    [feature] create method for fill every input in registration form

--- src/main/java/com/project/pages/RegistrationPage.java
+++ src/main/java/com/project/pages/RegistrationPage.java
@@ -1,10 +1,20 @@
 package com.project.pages;
 
+import com.project.locators.RegistrationLocators;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationPage extends BasePage {
+public class RegistrationPage extends BasePage implements RegistrationLocators {
 
     public RegistrationPage(WebDriver driver) {
         super(driver);
     }
+
+    @Step("Ввод во все поля на странице регистрации")
+    public void fillToRegistrationFields(String textName, String textEmail, String textPassword){
+        fillToInput(inputName, textName);
+        fillToInput(inputEmail, textEmail);
+        fillToInput(inputPassword, textPassword);
+    }
+
 }
