6894584a0799018ce534cc7b1c7c931cce073ffc
Magistrat <nisraton@gmail.com>
2023-10-04 23:23:24 +0300

    [feature] add method and locators for check ProfilePage

--- src/main/java/com/pageobject/locators/ProfileLocators.java
+++ src/main/java/com/pageobject/locators/ProfileLocators.java
@@ -1,4 +1,12 @@
 package com.pageobject.locators;
 
+import org.openqa.selenium.By;
+
 public interface ProfileLocators {
+    public static final By buttonForSaveInProfile = By.xpath(".//button[text() = 'Сохранить']");
+    public static final By buttonForCancelInProfile = By.xpath(".//button[text() = 'Отмена']");
+    public static final By buttonProfileInProfile = By.xpath(".//a[text() = 'Профиль']");
+    public static final By buttonOrdersHistoryInProfile = By.xpath(".//a[text() = 'История заказов']");
+    public static final By buttonLogOutInProfile = By.xpath(".//button[text() = 'Выход']");
+
 }
--- src/main/java/com/pageobject/pages/ProfilePage.java
+++ src/main/java/com/pageobject/pages/ProfilePage.java
@@ -1,11 +1,28 @@
 package com.pageobject.pages;
 
+import com.pageobject.locators.ProfileLocators;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
-public class ProfilePage extends HeaderPage{
+public class ProfilePage extends HeaderPage implements ProfileLocators {
 
     public ProfilePage(WebDriver driver) {
         super(driver);
     }
 
+    @Step("Проверка страницы Личный Кабинет")
+    public void checkProfilePagePage() {
+        checkHeaderPage();
+
+        elementHasVisibility(buttonForSaveInProfile);
+        elementHasVisibility(buttonForCancelInProfile);
+        elementHasVisibility(buttonProfileInProfile);
+        elementHasVisibility(buttonOrdersHistoryInProfile);
+        elementHasVisibility(buttonLogOutInProfile);
+    }
+
+    @Step("Выход из учетной записи")
+    public void clickLogOutInProfile(){
+        clickToElement(buttonLogOutInProfile);
+    }
 }
