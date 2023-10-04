951ab291813ce7df1426b1cbd55108824cd42384
Magistrat <nisraton@gmail.com>
2023-10-04 22:52:20 +0300

    [feature] create ProfilePage class and locators

---
+++ src/main/java/com/pageobject/pages/ProfilePage.java
@@ -0,0 +1,11 @@
+package com.pageobject.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProfilePage extends HeaderPage{
+
+    public ProfilePage(WebDriver driver) {
+        super(driver);
+    }
+
+}
