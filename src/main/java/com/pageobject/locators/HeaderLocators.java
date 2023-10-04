481273810de5c610178d571c2c4ef1e2fde38938
Magistrat <nisraton@gmail.com>
2023-10-04 23:09:30 +0300

    [feature] add locators and methods for check and click for header page

--- src/main/java/com/pageobject/locators/HeaderLocators.java
+++ src/main/java/com/pageobject/locators/HeaderLocators.java
@@ -1,4 +1,10 @@
 package com.pageobject.locators;
 
+import org.openqa.selenium.By;
+
 public interface HeaderLocators {
+    public static final By headerConstructorButton = By.xpath(".//p[text() = 'Конструктор']/..");
+    public static final By headerOrderListButton = By.xpath(".//p[text() = 'Лента Заказов']/..");
+    public static final By headerWebSiteLogoButton = By.cssSelector("a.active svg");
+    public static final By headerProfileButton = By.xpath(".//p[text() = 'Личный Кабинет']/..");
 }
--- src/main/java/com/pageobject/pages/HeaderPage.java
+++ src/main/java/com/pageobject/pages/HeaderPage.java
@@ -1,13 +1,41 @@
 package com.pageobject.pages;
 
+import com.pageobject.locators.HeaderLocators;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
-public class HeaderPage extends BasePage{
+public class HeaderPage extends BasePage implements HeaderLocators {
     // Шапка страницы
 
     public HeaderPage(WebDriver driver) {
         super(driver);
     }
 
+    @Step("Проверка Header (шапки) сайта")
+    public void checkHeaderPage(){
+        elementHasVisibility(headerConstructorButton);
+        elementHasVisibility(headerOrderListButton);
+        elementHasVisibility(headerWebSiteLogoButton);
+        elementHasVisibility(headerProfileButton);
+    }
 
+    @Step("Клик на кнопку Конструктор из Header (шапки) сайта")
+    public void clickToConstructorButtonFromHeader() {
+        clickToElement(headerConstructorButton);
+    }
+
+    @Step("Клик на кнопку Лента Заказов из Header (шапки) сайта")
+    public void clickToOrderListButtonFromHeader() {
+        clickToElement(headerOrderListButton);
+    }
+
+    @Step("Клик на Логотип Сайта из Header (шапки) сайта")
+    public void clickToWebSiteLogoButtonFromHeader() {
+        clickToElement(headerWebSiteLogoButton);
+    }
+
+    @Step("Клик на кнопку Личный Кабинет из Header (шапки) сайта")
+    public void clickToProfileButtonFromHeader() {
+        clickToElement(headerProfileButton);
+    }
 }
